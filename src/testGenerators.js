929419c67896657f37b90fbb2fcb61c411db59d9
jhowlett-scottlogic <107993905+jhowlett-scottlogic@users.noreply.github.com>
2022-09-27 14:58:51 +0100

    test: Add testing to CI CD (#65)
    * Add eslint as a dependency
    Add github actions workflow files
    for formating and linting.
    add new npm scripts for prettier and eslint
    
    * move workflows into .github folder
    
    * change workflow files to use 'master' not 'main'
    
    * Revert "Add eslint as a dependency"
    
    * add test.yml workflow
    prevent expansion of regex by using quotes in shell command.
    use process.exit(code) to let github actions know of test results
    
    * add continue-on-error
    
    * add test:generators script
    
    * Update package.json
    
    * wd
    
    * ss
    
    * Change GitHub URL to generator back to ScottLogic

--- src/testGenerators.js
+++ src/testGenerators.js
@@ -25,7 +25,7 @@ function setupAndStartTests(generatorPath, arg1, arg2) {
     shell.cd(generatorPath, log.shellOptions);
 
     log.standard("Starting tests");
-    const test = shell.exec(`npm run test ${arg1} ${arg2}`, log.shellOptions);
+    const test = shell.exec(`npm run test "${arg1}" "${arg2}"`, log.shellOptions);
 
     shell.cd(__dirname, log.shellOptions);
     return test.stdout.split("\n");
@@ -55,6 +55,7 @@ function getGenerator(languageData, generatorOption) {
 
 async function testGenerators(options) {
     let resultArray = {};
+    let exitCode = 0;
 
     log.setLogLevel(options.logLevel);
 
@@ -77,12 +78,18 @@ async function testGenerators(options) {
 
             const stdout = setupAndStartTests(generatorPath, featurePath, basePath);
 
-            resultArray.TypeScript = testResultParser.parseTypeScript(stdout[stdout.length-2], stdout[stdout.length-4]);
+            const result = testResultParser.parseTypeScript(stdout[stdout.length-2], stdout[stdout.length-4]);
+            
+            // check if failed/skipped/undefined steps in tests. If so OR them onto the exit code to stop overwriting previous errors
+            exitCode = exitCode | testResultParser.checkTestResultForErrors(result);
+
+            resultArray.TypeScript = result;
 
             log.standard(`${typescriptData.languageString} testing complete`);
 
         } catch(exception) {
             log.logFailedTesting(typescriptData.languageString, exception);
+            exitCode = exitCode | 1;
         } finally {
             generatorResolver.cleanup();
         }
@@ -92,16 +99,26 @@ async function testGenerators(options) {
         try {
             const generatorPath = getGenerator(csharpData, options.csharp);
 
-            const featurePath = path.relative(path.join(generatorPath, "tests/FeaturesTests"), path.join(__dirname, "..\\features\\*.feature"));
+            const featurePath = path.relative(path.join(generatorPath, "tests/FeaturesTests"), path.join(__dirname, "../features/*.feature"));
+
+            console.log("generatorPath:" + generatorPath);
+            console.log("__dirname:" + __dirname);
+            console.log("featurePath:" + featurePath);
 
             const stdout = setupAndStartTests(generatorPath, featurePath, "");
 
-            resultArray.CSharp = testResultParser.parseCSharp(stdout[stdout.length-2]);
+            const result = testResultParser.parseCSharp(stdout[stdout.length-2]);
+
+            // check if failed/skipped/undefined steps in tests. If so OR them onto the exit code to stop overwriting previous errors
+            exitCode = exitCode | testResultParser.checkTestResultForErrors(result);
+
+            resultArray.CSharp = result;
 
             log.standard(`${csharpData.languageString} testing complete`);
 
         } catch(exception) {
             log.logFailedTesting(csharpData.languageString, exception);
+            exitCode = exitCode | 1;
         } finally {
             generatorResolver.cleanup();
         }
@@ -110,6 +127,7 @@ async function testGenerators(options) {
     if(Object.keys(resultArray).length) {
         if(!log.isQuiet()) console.table(resultArray);
     }
+    process.exit(exitCode);
 }
 
 module.exports = testGenerators;
