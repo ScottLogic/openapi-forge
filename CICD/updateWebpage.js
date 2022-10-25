b8b77e44c570dc5f0554c13cfe18011b7411cd11
jhowlett-scottlogic <107993905+jhowlett-scottlogic@users.noreply.github.com>
2022-10-25 09:53:45 +0100

    ci: fix formatting errors

--- CICD/updateWebpage.js
+++ CICD/updateWebpage.js
@@ -2,30 +2,29 @@ const fs = require("fs");
 
 const log = require("../src/log.js");
 
-  // This regex extracts the generator table from README.md.
-  const tableRegex =
-  /\[MARKER\]:\s<>\s\(START\sOF\sGENERATOR\sTESTING\sTABLE\)([\s\S]+)\[MARKER\]:\s<>\s\(END\sOF\sGENERATOR\sTESTING\sTABLE\)/m; 
+// This regex extracts the generator table from README.md.
+const tableRegex =
+  /\[MARKER\]:\s<>\s\(START\sOF\sGENERATOR\sTESTING\sTABLE\)([\s\S]+)\[MARKER\]:\s<>\s\(END\sOF\sGENERATOR\sTESTING\sTABLE\)/m;
 
-  // This regex extracts the generator failures from README.md.
-  const failuresRegex =
-  /\[MARKER\]:\s<>\s\(START\sOF\sGENERATOR\sTESTING\sFAILURES\)([\s\S]+)\[MARKER\]:\s<>\s\(END\sOF\sGENERATOR\sTESTING\sFAILURES\)/m; 
+// This regex extracts the generator failures from README.md.
+const failuresRegex =
+  /\[MARKER\]:\s<>\s\(START\sOF\sGENERATOR\sTESTING\sFAILURES\)([\s\S]+)\[MARKER\]:\s<>\s\(END\sOF\sGENERATOR\sTESTING\sFAILURES\)/m;
 
-  function getNewFailures(results) {
-  let newFailures =
-  "[MARKER]: <> (START OF GENERATOR TESTING FAILURES)\n\n";
+function getNewFailures(results) {
+  let newFailures = "[MARKER]: <> (START OF GENERATOR TESTING FAILURES)\n\n";
 
   const languages = Object.keys(results);
 
   for (let xx = 0; xx < languages.length; xx++) {
     const result = results[languages[xx]];
     newFailures += `\n\n## ${languages[xx]}\n\n`;
-    if(result.failures) {
-        for (let yy = 0; yy < result.failures.length; yy++) {
-            newFailures += `- ${result.failures[yy]}\\\n`;
-        }
-        newFailures = newFailures.slice(0, -2);
+    if (result.failures) {
+      for (let yy = 0; yy < result.failures.length; yy++) {
+        newFailures += `- ${result.failures[yy]}\\\n`;
+      }
+      newFailures = newFailures.slice(0, -2);
     } else {
-        newFailures += "- No failed scenarios";
+      newFailures += "- No failed scenarios";
     }
   }
 
@@ -51,41 +50,37 @@ function getNewTable(results) {
 }
 
 function populateWebpage(results) {
-  
-    let fileContents = fs.readFileSync("WEBPAGE.md", "utf-8");
+  let fileContents = fs.readFileSync("WEBPAGE.md", "utf-8");
 
-    fileContents = fileContents.replace(tableRegex, getNewTable(results));
-  
-    fileContents = fileContents.replace(failuresRegex, getNewFailures(results));
+  fileContents = fileContents.replace(tableRegex, getNewTable(results));
 
-    fs.writeFileSync("WEBPAGE.md", fileContents);
+  fileContents = fileContents.replace(failuresRegex, getNewFailures(results));
+
+  fs.writeFileSync("WEBPAGE.md", fileContents);
 }
 
 //--------------------------START OF SCRIPT----------------------------------//
 
-
 function updateWebpage() {
-    log.setLogLevel(log.logLevels.verbose);
+  log.setLogLevel(log.logLevels.verbose);
 
-    // Extract cl arguments
-    const clArgs = process.argv.slice(2);
-    
-    if (clArgs.length !== 1) {
-        log.error("Incorrect number of arguments");
-        process.exit(1);
-    }
-    let results;
-    try {
-        results = JSON.parse(fs.readFileSync(clArgs[0], "utf-8"));
-        log.verbose(
-          `${log.underline}Test results${log.resetStyling}`
-        );
-        log.verbose(results);
-    } catch (ee) {
-        log.error(clArgs[0] + " : " + ee.message);
-        process.exit(1);
-    }
-    populateWebpage(results);
+  // Extract cl arguments
+  const clArgs = process.argv.slice(2);
+
+  if (clArgs.length !== 1) {
+    log.error("Incorrect number of arguments");
+    process.exit(1);
+  }
+  let results;
+  try {
+    results = JSON.parse(fs.readFileSync(clArgs[0], "utf-8"));
+    log.verbose(`${log.underline}Test results${log.resetStyling}`);
+    log.verbose(results);
+  } catch (ee) {
+    log.error(clArgs[0] + " : " + ee.message);
+    process.exit(1);
+  }
+  populateWebpage(results);
 }
 
 updateWebpage();
